54bd963e5a41bc24fe49b33ecae5a7fddafed88b
Carlos Barcos <fbarcos8@hotmail.com>
2023-03-29 01:13:04 -0500

    Agrgando validaciones

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,33 @@
 import { TaskStatus } from '../task.entity';
+import {
+  IsIn,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   title: string;
+
+  @IsString()
   description: string;
 }
 
 export class UpdatedTaskDto {
+  @IsString()
+  @IsOptional()
   title?: string;
+
+  @IsString()
+  @IsOptional()
   desription?: string;
+
+  @IsString()
+  @IsOptional()
+  @IsIn([TaskStatus.PENDING, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status?: TaskStatus;
 }
